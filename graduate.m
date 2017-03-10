0197872c1d31542a13de14d7a93f8d0195c96534
tjcemjz@gmail.com <tjcemjz@gmail.com>
2017-03-10 15:06:37 +0800

    729daily change

--- graduate.m
+++ graduate.m
@@ -36,8 +36,8 @@ end
 PARADIR = [1 1 -1 1 -1];
 
 
-totalNum = 7500;
-lowerBound = -4;
+totalNum = 50;
+lowerBound = -3;
 upperBound = 0.25;
 lbdir = lowerBound*PARADIR;
 ubdir = upperBound*PARADIR;
@@ -58,6 +58,7 @@ TEMPLOC = [exeFolder '\temp.txt'];
 INILOC = [exeFolder '\flac.ini'];
 FLACCMD = 'call ';
 FLACF_NAME = ['rainfallfos1.dat';'rainfallfos2.dat';'rainfallfos3.dat';'rainfallfos4.dat'];
+%FLACF_NAME = ['rainfall1.dat';'rainfall2.dat';'rainfall3.dat';'rainfall4.dat'];
 RSTF_NAME = ['result1.txt';'result2.txt';'result3.txt';'result4.txt'];
 FLAG = 4;
 % program settings
@@ -67,16 +68,26 @@ FLAG = 4;
 % rainfall amount: unit 1e-4
 % rainfall time: unit hour
 % initial suction: unit kpa ; positive
-sstv = [1 4 5];
+sstv = [0.75 2 5
+        1.25 2 5
+        0.25 2 5
+        0.5 2 5
+        1 5 5
+        1 3 5
+        1 4 15
+        1 4 25
+        1 4 20
+        1 1 5];
 [sstvm,sstvn] = size(sstv);
 
 for sstv_id = 1:sstvm
     
   testl = 0;
-  testu = 3;
-  orgPara = betascreen(orgPara_notscreened,testl,testu);
-  size(orgPara)
-
+  testu = 3.5;
+  %orgPara = betascreen(orgPara_notscreened,testl,testu);
+  %size(orgPara)
+  orgPara = orgPara_notscreened;
+  
   % program parameters
   circlePara = addNum(orgPara);
   % program parameters
